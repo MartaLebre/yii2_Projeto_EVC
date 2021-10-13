0646c9a20b29e48b07e48d863f4bc8c472ff05b3
Joao Pereira <joao.pereira@onesource.pt>
2021-10-13 10:45:25 +0100

    RBAC

---
+++ console/migrations/m211013_084803_init_rbac.php
@@ -0,0 +1,111 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211013_084803_init_rbac
+ */
+class m211013_084803_init_rbac extends Migration
+{
+
+
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+        $auth = Yii::$app->authManager;
+
+
+        $adicionarGestor = $auth->createPermission('adicionarGestor');
+        $adicionarGestor->description = 'Adicionar Gestor';
+        $auth->add($adicionarGestor);
+
+        $editarGestor = $auth->createPermission('editarGestor');
+        $editarGestor->description = 'Editar Gestor';
+        $auth->add($editarGestor);
+
+        $apagarGestor = $auth->createPermission('apagarGestor');
+        $apagarGestor->description = 'Apagar Gestor';
+        $auth->add($apagarGestor);
+
+        $bloquearCliente = $auth->createPermission('bloquearCliente');
+        $bloquearCliente->description = 'Bloquear Cliente';
+        $auth->add($bloquearCliente);
+
+        $desbloquearCliente = $auth->createPermission('desbloquearCliente');
+        $desbloquearCliente->description = 'Desbloquear Cliente';
+        $auth->add($desbloquearCliente);
+
+        $adicionarProdutos = $auth->createPermission('adicionarProdutos');
+        $adicionarProdutos->description = 'Adicionar Produtos';
+        $auth->add($adicionarProdutos);
+
+        $editarProdutos = $auth->createPermission('editarProdutos');
+        $editarProdutos->description = 'Editar Produtos';
+        $auth->add($editarProdutos);
+
+        $eliminarProdutos = $auth->createPermission('eliminarProdutos');
+        $eliminarProdutos->description = 'Eliminar Produtos';
+        $auth->add($eliminarProdutos);
+
+        $adicionarProdutosFavoritos = $auth->createPermission('adicionarProdutosFavoritos');
+        $adicionarProdutosFavoritos->description = 'Adicionar Produtos aos Favoritos';
+        $auth->add($adicionarProdutosFavoritos);
+
+        $encomendarProdutos = $auth->createPermission('encomendarProdutos');
+        $encomendarProdutos->description = 'Encomendar Produtos';
+        $auth->add($encomendarProdutos);
+
+        $adicinarProdutosAoCarrinho = $auth->createPermission('adicinarProdutosAoCarrinho');
+        $adicinarProdutosAoCarrinho->description = 'Adicionar Produtos ao Carinnho';
+        $auth->add($adicinarProdutosAoCarrinho);
+
+        $pesquisarProdutos = $auth->createPermission('pesquisarProdutos');
+        $pesquisarProdutos->description = 'Pesquisar Produtos';
+        $auth->add($pesquisarProdutos);
+
+        $visualizarProdutos = $auth->createPermission('visualizarProdutos');
+        $visualizarProdutos->description = 'Visualizar Produtos';
+        $auth->add($visualizarProdutos);
+
+
+        $cliente = $auth->createRole('cliente');
+        $auth->add($cliente);
+        $auth->addChild($cliente, $pesquisarProdutos);
+        $auth->addChild($cliente, $encomendarProdutos);
+        $auth->addChild($cliente, $adicinarProdutosAoCarrinho);
+        $auth->addChild($cliente, $adicionarProdutosFavoritos);
+        $auth->addChild($cliente, $visualizarProdutos);
+
+
+        $gestorStock = $auth->createRole('gestorStock');
+        $auth->add($gestorStock);
+        $auth->addChild($gestorStock, $adicionarProdutos);
+        $auth->addChild($gestorStock, $editarProdutos);
+        $auth->addChild($gestorStock, $eliminarProdutos);
+        $auth->addChild($gestorStock, $pesquisarProdutos);
+        $auth->addChild($gestorStock, $visualizarProdutos);
+
+
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $adicionarGestor);
+        $auth->addChild($admin, $editarGestor);
+        $auth->addChild($admin, $apagarGestor);
+        $auth->addChild($admin, $bloquearCliente);
+        $auth->addChild($admin, $desbloquearCliente);
+
+
+        $auth->assign($admin, 1);
+    }
+
+
+
+    public function down()
+    {
+        $auth = Yii::$app->authManager;
+
+        $auth->removeAll();
+    }
+
+}
