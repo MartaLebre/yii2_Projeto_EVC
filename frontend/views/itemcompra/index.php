fc2b4102b6612f831dbbc63ced07300ba4843898
JoaoPereira5 <2190726@my.ipleiria.pt>
2021-12-07 21:17:01 +0000

    index carrinho

--- frontend/views/itemcompra/index.php
+++ frontend/views/itemcompra/index.php
@@ -14,26 +14,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Item Compra', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'codigo_produto',
-            'id_encomenda',
-            'quantidade',
-            'preço_venda',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
 
 
 </div>
