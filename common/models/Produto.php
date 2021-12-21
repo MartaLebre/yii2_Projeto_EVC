7a63c9b7aa964a947337822854e478458811c60a
Joao Pereira <2190726@my.ipleiria.pt>
2021-12-21 17:51:58 +0000

    model produto com o campo da foto

--- common/models/Produto.php
+++ common/models/Produto.php
@@ -16,10 +16,7 @@ use Yii;
  * @property int $quantidade
  * @property string $data
  * @property int $id_modelo
- * @property resource|null $foto
- *
- * @property Modelo $modelo
- * @property Favorito $favorito
+ * @property string|null $foto
  */
 class Produto extends \yii\db\ActiveRecord
 {
@@ -39,11 +36,12 @@ class Produto extends \yii\db\ActiveRecord
         return [
             [['codigo_produto', 'nome', 'genero', 'descricao', 'tamanho', 'preco', 'quantidade', 'data', 'id_modelo'], 'required'],
             [['codigo_produto', 'quantidade', 'id_modelo'], 'integer'],
-            [['genero', 'tamanho', 'foto'], 'string'],
+            [['genero', 'tamanho'], 'string'],
             [['preco'], 'number'],
             [['data'], 'safe'],
             [['nome'], 'string', 'max' => 45],
             [['descricao'], 'string', 'max' => 90],
+            [['foto'], 'string', 'max' => 50],
             [['codigo_produto'], 'unique'],
         ];
     }
@@ -57,9 +55,9 @@ class Produto extends \yii\db\ActiveRecord
             'codigo_produto' => 'Codigo Produto',
             'nome' => 'Nome',
             'genero' => 'Genero',
-            'descricao' => 'Descrição',
+            'descricao' => 'Descricao',
             'tamanho' => 'Tamanho',
-            'preco' => 'Preço',
+            'preco' => 'Preco',
             'quantidade' => 'Quantidade',
             'data' => 'Data',
             'id_modelo' => 'Id Modelo',
@@ -71,9 +69,10 @@ class Produto extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Modelo::className(), ['id' => 'id_modelo']);
     }
-    
+
     public function getFavorito()
     {
         return $this->hasOne(Favorito::className(), ['codigo_produto' => 'codigo_produto']);
     }
 }
+
